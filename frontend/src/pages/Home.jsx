e7fe3db723247a3d67bb447ed15ada7dbc6031e7
Yusuf Tan <yusuftan41@hotmail.com>
2024-10-21 21:02:15 +0300

    Popular products added to the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero"
+import Popular from "../components/Popular"
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
