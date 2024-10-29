e4dc58284e31814070836e5b8c39262c4a8f9e7d
Yusuf Tan <yusuftan41@hotmail.com>
2024-10-29 23:33:48 +0300

    Product Detail Page planned and development.

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import { createContext } from "react";
-import all_product from "../assets/all_products"
+import all_products from "../assets/all_products"
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-    const contextValue = {all_product};
+    const contextValue = {all_products};
 
     return(
         <ShopContext.Provider value={contextValue}>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useContext } from 'react'
+import {ShopContext} from "../Context/ShopContext"
+import { useParams } from 'react-router-dom' 
+import ProductHd from '../components/ProductHd'
+import ProductDisplay from '../components/ProductDisplay'
 
 const Product = () => {
+  const {all_products} = useContext(ShopContext)
+  const {productId} = useParams()
+  const product = all_products.find((e) => e.id === Number(productId))
+  if(!product){
+    return <div>Product not found!</div>
+  }
+
   return (
-    <div>
-      
-    </div>
+    <section className='max_padd_container py-28'>
+      <div>
+        <ProductHd product={product}/>
+        <ProductDisplay product={product}/>
+      </div>
+    </section>
   )
 }
 
