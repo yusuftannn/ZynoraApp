b1988e7e103e86a3794ac4aa50c0cb90d04262a3
Yusuf Tan <yusuftan41@hotmail.com>
2024-11-06 16:35:04 +0300

    Added API endpoints for adding, removing, and retrieving products, and modified upload endpoint.

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage:storage})
+
 // creating upload endpoint for images
 app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
@@ -71,6 +72,52 @@ const Product = mongoose.model("Product", {
         type: Boolean,
         default: true,
     }
+})
+
+// creating api for add products
+
+app.post('/addproduct', async(req, res) => {
+    let products = await Product.find({})
+    let id
+    if(products.length > 0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id +1;
+    }else{
+        id = 1;
+    }
+    const product = new Product({
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price
+    })
+    console.log(product);
+    await product.save();
+    console.log("Saved")
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// creating api for add products
+app.post('/removeproduct', async(req, res) => {
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name: req.body.name,
+    })
+})
+
+// Creating api for get all products
+app.get('/allproducts', async(req, res) => {
+    let products = await Product.find({})
+        console.log("All products Fetched")
+        res.send(products);
 })
 
 app.listen(port, (error) => {
