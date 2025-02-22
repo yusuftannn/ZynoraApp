42c8fcb3b1d6986d501710e67df6a9a0cf675642
Yusuf Tan <yusuftan41@hotmail.com>
2025-02-22 17:45:20 +0300

    Added user registration and login functionality with JWT authentication.

--- backend/index.js
+++ backend/index.js
@@ -120,6 +120,74 @@ app.get('/allproducts', async(req, res) => {
         res.send(products);
 })
 
+// Schema user model
+const User = mongoose.model('User', {
+    name: {
+        type:String,
+    },
+    email: {
+        type:String,
+        unique:true,
+    },
+    password: {
+        type:String,
+    },
+    carData: {
+        type:Object,
+    },
+    date: {
+        type:Date,
+        default:Date.now,
+    },
+})
+
+// Creating endpoint for registering the user
+app.post('/signup', async(req,res) => {
+    let check = await User.findOne({email: req.body.email});
+    if(check){
+        return res.status(400).json({success:false, errors: "Existing user fount with same email address"});
+    }
+    let cart = {}
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        carData: cart,
+    })
+    await user.save();
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success:true, token:token})
+})
+
+// Creating endpoint for user login
+app.post('/login', async(req,res) => {
+    let user = await User.findOne({email:req.body.email});
+    if(user){
+        const passMatch = req.body.password === user.password;
+        if(passMatch){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({success:true, token})
+        } else{
+            res.json({success:false, errors:"Wrong Password"});
+        }
+    } else{
+        res.json({success:false, errors:"Wrong Email Address"});
+    }
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server is running on port" + port);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,72 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+ 
+  const login = async () => {
+    console.log("login", formData)
+    let responseData;
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers:{
+        Accept: 'application/formData',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+     }).then((response)=>response.json()).then((data)=>responseData=data)
+
+     if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token)
+      window.location.replace('/')
+     }
+     else{
+      alert(responseData.errors)
+     }
+  }
+  const signup = async () => {
+    console.log("signup", formData)
+    let responseData;
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers:{
+        Accept: 'application/formData',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+     }).then((response)=>response.json()).then((data)=>responseData=data)
+
+     if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token)
+      window.location.replace('/')
+     }
+     else{
+      alert(responseData.errors)
+     }
+  }
+
   return (
     <section className='max_padd_container flexCenter flex-col pt-32'>
       <div className='max-w-[555px] h-[600px] bg-white m-auto px-14 py-10 rounded-md'>
-        <h3 className='h3'>Sign Up</h3>
+        <h3 className='h3'>{state}</h3>
         <div className='flex flex-col gap-4 mt-7'>
-          <input type="text" placeholder="Your Name" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
-          <input type="email" placeholder="Email Address" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
-          <input type="password" placeholder="Password" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
+          {state==="Sign Up"?<input name="username" value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>:''}
+          <input name="email" value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
+          <input name="password" value={formData.password} onChange={changeHandler} type="password" placeholder="Password" className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
         </div>
-        <button className='btn_dark_rounded my-4 w-full !rounded-md'>Continue</button>
-        <p className='text-black font-bold'>Already have an account? <span className='text-secondary underline cursor-pointer'>Login</span></p>
+        <button onClick={()=>{state==="Login"?login():signup()}} className='btn_dark_rounded my-4 w-full !rounded-md'>Continue</button>
+        {state === "Sign Up"
+        ? <p className='text-black font-bold'>Already have an account? <span onClick={()=>{setState("Login")}} className='text-secondary underline cursor-pointer'> Login</span></p>
+        : <p className='text-black font-bold'>Create an account? <span onClick={()=>{setState("Sign Up")}} className='text-secondary underline cursor-pointer'> Sign Up</span></p>}
         <div className='flexCenter mt-6 gap-3'>
           <input type="checkbox" name="" id=""/>
           <p>By continuing, i agreee to the terms of use & privacy policy.</p>
